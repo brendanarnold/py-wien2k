b211a13cc81ae9d409f276bf1986bc30e09217d5
brendanarnold <none@none>
2009-12-31 15:48:04 +0000

    FIXED: Stopped utils module loading the oq deleted build_cubic_k_grid FIXED: expand_irreducible_brillouin_zone now works with new SymmetryMatrix objects

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,2 @@
 from expand_irreducible_brillouin_zone import expand_irreducible_brillouin_zone
-from build_cubic_k_grid import build_cubic_k_grid
 from extract_isoenergy_mesh import extract_isoenergy_mesh
--- utils/expand_irreducible_brillouin_zone.py
+++ utils/expand_irreducible_brillouin_zone.py
@@ -5,8 +5,8 @@ def expand_irreducible_brillouin_zone(symmetry_matrices, k_points):
     for k_point in k_points:
         tmp_k_points = []
         for symmetry_matrix in symmetry_matrices:
-            symmetry_point = list(np.dot(k_point[1:4], symmetry_matrix.matrix))
-            symmetry_point = symmetry_point + symmetry_matrix.tau_offsets
+            symmetry_point = list(np.dot(k_point[1:4], symmetry_matrix.matrix) \
+                    + symmetry_matrix.tau_offsets)
             tmp_k_points.append(symmetry_point)
         # == Remove duplicates and add the k point id and any further columns if any ==
         tmp_k_points.sort()
