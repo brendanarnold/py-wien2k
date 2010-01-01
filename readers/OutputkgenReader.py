5fe65a2dcef513003e33dfe25cbd87c4ae88f18d
brendanarnold <none@none>
2010-01-01 20:27:53 +0000

    Laid out OutputkgenReader so will be easier to flesh out now reads in point group symmetry matrices

--- readers/OutputkgenReader.py
+++ readers/OutputkgenReader.py
@@ -11,75 +11,160 @@ from wien2k.errors import UnexpectedFileFormat
 from wien2k.SymmetryMatrix import SymmetryMatrix
 
 fmt = {
-    # Skip these line until find out what they are
-    'skip_lines' : 11,
-    # A string that denotes the line before the symmetry matrices
-    'matrix_header_test_string' : 'SYMMETRY MATRIX NR.',
-    # A string that denotes the line before the rectangular lattice vectors
-    'rec_lattice_vectors_header_test_string' : 'G1        G2        G3'
+    'ortho_line_num' : 1,
+    'r1_vector_line_num' : 2, 
+    'iarb_line_num' : 5,
+    'point_group_symmetries_line_num' : 6,
+    'symmetry_matrix_header_line_startswith' : 'SYMMETRY MATRIX NR.',
+    'reciprocal_lattice_vectors_header_line_startswith' : 'G1        G2        G3',
+    'length_reciprocal_lattice_vectors_line_startswith' : 'length of reciprocal lattice vectors:',
+    'submesh_shift_line_startswith' : 'SUBMESH SHIFTED; SHIFT:',
+    'number_mesh_points_line_startswith' : 'NO. OF MESH POINTS IN THE BRILLOUIN ZONE =',
+    'reciprocal_lattice_vector_intervals_line_startswith' : 'DIVISION OF RECIPROCAL LATTICE VECTORS (INTERVALS)=',
+    'num_inequiv_k_points_line_startswith' : 'NO. OF INEQUIVALENT K-POINTS',
+    'len_bloch_vector_line' : 74,
+    'tetrahedra_to_sort_line_startswith' : 'tetrahedra to sort:',
+    'owork_line_startswith' : 'owork:',
+    'number_different_tetrahedra_line_startwith' : 'NUMBER OF DIFFERENT TETRAHEDRA :',
+    'number_k_points_div_afact_line_startswith' : 'NKP,NDIV,afact',
+    'len_tretrahedra_point_line' : 85,
 }
-
- 
-
 
 class OutputkgenReader(object):
     '''A class which reads from the .outputkgen file which contains symmetry vectors'''
     def __init__(self, filename):
         self.filename = filename
+        self.reciprocal_lattice_vectors = []
+        self.reciprocal_lattice_vectors_by_2pi = []
         self.symmetry_matrices = []
-        self.rectangular_lattice_vectors = []
-        # Read in the symmetry matrices
-        file_handle = open(filename, 'r')
-        line_num = 0
-        while True:
-            line = file_handle.readline()
-            line_num = line_num + 1
-            # Test for EOF
-            if line == '':
-                break
-            # Skip a certain number of lines - first row of matrices contain unknown data
-            if line_num < fmt['skip_lines']:
-                continue
-            # Test to see if a set of matrices is coming up
-            if line.strip().startswith(fmt['matrix_header_test_string']):
-                tmp_matrices = []
-                # Read the matrices off the next three lines into a buffer
-                for i in [1,2,3]:
-                    line = file_handle.readline()
-                    vals = [int(x.strip()) for x in line.split(' ') if x.strip() != '']
-                    # Count how many matrices required (may not be three)
-                    num_matrices = len(vals) / 3
-                    for j in range(num_matrices):
-                        tmp_matrices.append([])
-                    for j in range(num_matrices):
-                        tmp_matrices[j].append(vals[3*j:3*(j+1)])
-                # Read the buffered matrices into the object as a Numpy array object only
-                # if the determinant is not zero
-                for i in range(num_matrices):
-                    tmp_matrix = np.array(tmp_matrices[i])
-                    if np.linalg.det(tmp_matrix) == 0:
-                        continue
-                    else:
-                        self.symmetry_matrices.append(SymmetryMatrix(matrix = tmp_matrix))
-            # Test to see if is line preceding the rectangular lattice vectors
-            if line.strip().startswith(fmt['rec_lattice_vectors_header_test_string']):
+        self.point_group_symmetry_matrices = []
+        self.bloch_vectors = None
+        self.tetrahedra = None
+        self._load_values()
+
+    def _load_values(self):
+        self._file_handle = open(self.filename, 'r')
+        self._line_num = 0
+        for line in self._file_handle:
+            self._line_num = self._line_num + 1
+            line = line.strip()
+
+            if self._line_num == fmt['ortho_line_num']:
+                #TODO
+                pass
+
+            elif self._line_num == fmt['r1_vector_line_num']:
+                #TODO
+                line2 = self._file_handle.next()
+                line3 = self._file_handle.next()
+                self._line_num = self._line_num + 2
+
+            elif self._line_num == fmt['iarb_line_num']:
+                #TODO
+                pass
+
+            elif self._line_num == fmt['point_group_symmetries_line_num']:
+                #TODO read in number of point group symmetry matrices
+                self._file_handle.next() # Throw away a buffer line
+                self.point_group_symmetry_matrices.extend(self._read_symmetry_matrices())
+                pass
+
+            elif line.startswith(fmt['symmetry_matrix_header_line_startswith']):
+                self.symmetry_matrices.extend(self._read_symmetry_matrices())
+
+            elif line.startswith(fmt['reciprocal_lattice_vectors_header_line_startswith']):
                 g1 = []
                 g2 = []
                 g3 = []
                 for i in [1,2,3]:
-                    line_num = line_num + 1
-                    line = file_handle.readline()
+                    self._line_num = self._line_num + 1
+                    try:
+                        line = self._file_handle.next()
+                    except StopIteration:
+                        raise UnexpectedFileFormat('Less than 3 lines were specified for the reciporcal lattice vectors (line: %d)' % self._line_num)
                     try:
                         vals = [float(x.strip()) for x in line.split(' ') if x.strip() != '']
                         g1.append(vals[0])
                         g2.append(vals[1])
                         g3.append(vals[2])
                     except:
-                        raise UnexpectedFileFormat('Could not parse rectangular lattice vectors from line (line: %d)' % line_num)
-                self.rectangular_lattice_vectors.append(np.array(g1))
-                self.rectangular_lattice_vectors.append(np.array(g2))
-                self.rectangular_lattice_vectors.append(np.array(g3))
+                        raise UnexpectedFileFormat('Could not parse reciprocal lattice vectors from line (line: %d)' % self._line_num)
+                # First set of reciprocal lattic vetors is followed by an
+                # identical set multiplied by 2*pi
+                if len(self.reciprocal_lattice_vectors) != 3:
+                    self.reciprocal_lattice_vectors.append(np.array(g1))
+                    self.reciprocal_lattice_vectors.append(np.array(g2))
+                    self.reciprocal_lattice_vectors.append(np.array(g3))
+                else:
+                    self.reciprocal_lattice_vectors_by_2pi.append(np.array(g1))
+                    self.reciprocal_lattice_vectors_by_2pi.append(np.array(g2))
+                    self.reciprocal_lattice_vectors_by_2pi.append(np.array(g3))
+
                 # Only take the first set of lattice vectors (second set is just 2*pi*a)
-                break
-            # TODO: Parse out other values
-        file_handle.close()
+
+            elif line.startswith(fmt['length_reciprocal_lattice_vectors_line_startswith']):
+                #TODO
+                pass
+
+            elif line.startswith(fmt['submesh_shift_line_startswith']):
+                #TODO
+                pass
+
+            elif line.startswith(fmt['number_mesh_points_line_startswith']):
+                #TODO
+                pass
+
+            elif line.startswith(fmt['reciprocal_lattice_vector_intervals_line_startswith']):
+                #TODO
+                pass
+
+            elif line.startswith(fmt['num_inequiv_k_points_line_startswith']):
+                #TODO
+                # fmt['len_bloch_vector_line']
+                pass
+
+            elif line.startswith(fmt['tetrahedra_to_sort_line_startswith']):
+                #TODO
+                pass
+
+            elif line.startswith(fmt['owork_line_startswith']):
+                #TODO
+                pass
+
+            elif line.startswith(fmt['number_different_tetrahedra_line_startwith']):
+                #TODO
+                pass
+
+            elif line.startswith(fmt['number_k_points_div_afact_line_startswith']):
+                #TODO
+                #'len_tretrahedra_point_line' : 85,
+                pass
+
+        self._file_handle.close()
+
+    def _read_symmetry_matrices(self):
+        tmp_matrices = []
+        # Read the matrices off the next three lines into a buffer
+        for i in [1,2,3]:
+            try:
+                line = self._file_handle.next()
+                self._line_num = self._line_num + 1
+            except StopIteration:
+                raise UnexpectedFileFormat( \
+                    'Symmetry matrices do not span 3 lines (line: %d)' % self._line_num)
+            vals = [int(x.strip()) for x in line.split(' ') if x.strip() != '']
+            # Count how many matrices required (may not be three)
+            num_matrices = len(vals) / 3
+            for j in range(num_matrices):
+                tmp_matrices.append([])
+                tmp_matrices[j].append(vals[3*j:3*(j+1)])
+        # Read the buffered matrices into the object as a Numpy array object only
+        # if the determinant is not zero
+        returned_matrices = []
+        for i in range(num_matrices):
+            tmp_matrix = np.array(tmp_matrices[i])
+            if np.linalg.det(tmp_matrix) == 0:
+                continue
+            else:
+                returned_matrices.append(SymmetryMatrix(matrix = tmp_matrix))
+        return returned_matrices
